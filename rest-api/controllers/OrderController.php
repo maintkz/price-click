771bfb67093aff11bc9a30590ddebe3bd444c3e3
3go-maniac <omashev.sultan@gmail.com>
2018-01-25 14:15:42 +0600

    asd

--- rest-api/controllers/OrderController.php
+++ rest-api/controllers/OrderController.php
@@ -36,7 +36,7 @@ class OrderController extends Controller
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         if(Yii::$app->request->isPost) {
 
-            return Yii::$app->request->post('auth_key');
+            return Yii::$app->request->post();
             die();
             $auth_key = Yii::$app->request->post('auth_key');
             if ($mUser = $mUser->getIdentityByAuthKey($auth_key)) {
